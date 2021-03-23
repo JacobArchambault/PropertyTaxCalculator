b12e14b87c3ee01e78874511d1b60cb855ba4f76
Jacob Archambault <jacobarchambault@gmail.com>
2021-03-23 08:39:07 -0400

    Delete buildButtonPanel() and move its logic into PropertyTaxPanel constructor

--- src/com/jacobarchambault/propertytaxcalculator/PropertyTaxPanel.java
+++ src/com/jacobarchambault/propertytaxcalculator/PropertyTaxPanel.java
@@ -14,58 +14,44 @@ public class PropertyTaxPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextField propertyValue; // To get property value
+	private JTextField propertyValue = new JTextField(
+			10); // To get property value
+	JButton calcButton = new JButton(
+			"Calculate Property tax");
 
 	/**
 	 * Constructor
 	 */
 	public PropertyTaxPanel() {
 		// Create a text field.
-		propertyValue = new JTextField(
-				10);
 		// Create a GridLayout manager.
 		setLayout(
 				new GridLayout(
-						2,
-						2));
+						3,
+						1));
 		// Add the labels and text fields to this panel.
 		add(
 				new JLabel(
 						"Enter the actual property value:"));
 		add(
 				propertyValue);
-		buildButtonPanel();
+		calcButton.addActionListener(
+				e -> showPropertyTax());
+		// Put the button in its own panel.
+		add(
+				calcButton,
+				BorderLayout.CENTER);
 	}
 
 	public void showPropertyTax() {
-		// Get the assessmentValue.
 		double assessmentValue = Double.parseDouble(
 				propertyValue.getText()) * .6;
-		// Get the property tax.
-		double propertyTax = assessmentValue * .0064;
-		// Display them.
 		JOptionPane.showMessageDialog(
 				null,
 				String.format(
 						"Assessment Value: $%,.2f\nProperty tax: $%,.2f",
 						assessmentValue,
-						propertyTax));
-	}
-	/**
-	 * The buildButtonPanel method creates a panel containing the calc property tax
-	 * button.
-	 */
-	private void buildButtonPanel() {
-		// Create a button to calculate the property tax.
-		JButton calcButton = new JButton(
-				"Calculate Property tax");
-		// Add an action listener to the button.
-		calcButton.addActionListener(
-				e -> showPropertyTax());
-		// Put the button in its own panel.
-		add(
-				calcButton, BorderLayout.CENTER);
-
+						assessmentValue * .0064));
 	}
 
 }
